57d651e4593b00b92462a2102f5fca0a097d21ba
nhnent <sw.talk@nhnent.com>
2018-10-14 23:38:28 +0900

    가게부 DB 연동 구현 데이터 받아와서, view 에 출력하도록 구현

--- expense/views.py
+++ expense/views.py
@@ -3,12 +3,47 @@ from expense.models import MobelTable
 from django.utils import timezone
 
 def expense_home(request):
+    p = MobelTable.objects.all()
+
     template = 'expense/expense_index.html'
-    return render(request, template)
+    return render(request, template, {'p':p})
 
 
 def expense_input(request):
+    if request.POST:
+        category = request.POST['category']
+        usedate = request.POST['usedate']
+        field = request.POST['field']
+        Subscribe = request.POST['Subscribe']
+        price = request.POST['price']
+        payment_logic = request.POST['payment_logic']
+        etc = request.POST['etc']
+        p = MobelTable(usedate=usedate, category=category,field=field,subscribe=Subscribe, price=price,payment_logic=payment_logic,etc=etc)
+        p.save()
+    else:
+        category = ''
+        usedate = ''
+        field = ''
+        Subscribe = ''
+        price =''
+        payment_logic =''
+        etc =''
     template = 'expense/expense_record.html'
+    return render(request, template, {'category': category,
+                                      'usedate': usedate,
+                                      'field': field,
+                                      'Subscribe': Subscribe,
+                                      'price': price,
+                                      'payment_logic': payment_logic,
+                                      'etc': etc})
 
-    return render(request, template)
-
+#
+#
+#
+# def loan_main(request):
+#     if request.POST.get("your name"):
+#         msg = request.POST['your name']
+#     else:
+#         msg = "NO"
+#     template = 'loan/loan_main.html'
+#     return render(request, template, {'msg': msg})

75d6e1c34cb898306e28444004281b55516db4f9
sw.talk <sw.talk@nhnent.com>
2018-10-28 18:34:11 +0900

    ETC form 구현

--- expense/forms.py
+++ expense/forms.py
@@ -1,17 +1,19 @@
 from django import forms
 import datetime
 
+
 class ExpenseForms(forms.Form):
     # your_name = forms.CharField(label='Your name', max_length=100)
-    usedate = forms.DateField(initial=datetime.date.today, widget=forms.TextInput(attrs={'class' : 'date-picker',
-                                                                                         'autocomplete' : 'off',
-                                                                                         'placeholder' : '날짜선택'}))
+    usedate = forms.DateField(initial=datetime.date.today, widget=forms.TextInput(attrs={'class': 'date-picker',
+                                                                                         'autocomplete': 'off',
+                                                                                         'placeholder': '날짜선택'}))
     category = forms.CharField(max_length=30)
     field = forms.CharField(max_length=30)
-    subscribe = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    price = forms.IntegerField(max_value=0, widget=forms.TextInput(attrs={'class' : 'form-control',
-                                                                          'placeholder' : '1,000,000',
-                                                                          'autocomplete' : 'off'}))
+    subscribe = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    price = forms.IntegerField(max_value=0, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                          'placeholder': '1,000,000',
+                                                                          'autocomplete': 'off'}))
     payment_logic = forms.CharField(max_length=30)
-    etc = forms.CharField(max_length=100)
-
+    etc = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                          'placeholder': '할인/적립 기타 등등',
+                                                                          'autocomplete': 'off'}))
